69d5e69f8b70f7bef05262188f73eddcf2c01241
DF <30237354+heracliteanflux@users.noreply.github.com>
2023-09-04 14:59:19 -0400

    updates

--- edxbc/D14_dataviz_2023/README.md
+++ edxbc/D14_dataviz_2023/README.md
@@ -2,7 +2,21 @@
 
 * [ [h](https://getbootstrap.com/) ][ [cdn](https://www.bootstrapcdn.com/) ] Bootstrap
 * [ [h](https://d3js.org/) ][ [cdn](https://d3js.org/d3.v7.min.js) ] D3.js
+  * [d3-selection](https://github.com/d3/d3-selection)
+    * [`d3.select`](https://github.com/d3/d3-selection#select)
+    * [`selection.append`](https://github.com/d3/d3-selection#selection_append)
+    * [`selection.html`](https://github.com/d3/d3-selection#selection_html)
+    * [`selection.property`](https://github.com/d3/d3-selection#selection_property)
+    * [`selection.text`](https://github.com/d3/d3-selection#selection_text)
+  * [d3-request](https://github.com/d3/d3-request/tree/master)
+    * [`d3.json`](https://github.com/d3/d3-request/tree/master#json)
 * [ h ][ [cdn](https://plotly.com/javascript/getting-started/) ] Plotly.js
 
-`d3.select`
-`d3.json`
+JavaScript
+* [ [d](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter) ] `Array.prototype.filter()`
+* [ [d](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/length) ] Array `length`
+* [ [d](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions) ] Arrow function expression
+* [ [d](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let) ] `let`
+* [ [d](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration) ] Loops and iteration
+* [ [d](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then) ] `Promise.prototype.then()`
+* [ [d](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals) ] Template literals
--- edxbc/D14_dataviz_2023/static/js/app.js
+++ edxbc/D14_dataviz_2023/static/js/app.js
@@ -1,21 +1,65 @@
-function buildMetadata (sample) {
+// samples.json is an `object` of three `arrays`
+//   names
+//   metadata (id, ehtnicity, gender, age, location, bbtype, wfreq)
+//   samples
 
+function buildMetadata (sample) {
+  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json")
+    .then((data) => {
+
+      let metadata    = data.metadata;                                        // an array of metadata objects
+      
+      // filter the data for the object with `sample`'s id
+      let resultArray = metadata.filter(sampleObj => sampleObj.id == sample); // an array of a single metadata object
+      let result      = resultArray[0];                                       // a single metadata object
+      let PANEL       = d3.select('#sample-metadata');                        // select tag `div` with id `sample-metadata`...
+          PANEL.html('');                                                     // ...and clear any preexisting metadata
+      
+      // iterate over the keys (id, ehtnicity, gender, age, location, bbtype, wfreq) of the metadata object
+      for (key in result) {                                                   // for each key...
+        PANEL
+          .append('h6')                                                       // ...create a tag `h6` under the sample-metadata tag
+          .text(`${key.toUpperCase()}: ${result[key]}`);                      // whose text content is the key and its value
+      }
+
+      buildGauge(result.wfreq);
+    });
 }
 
 function buildCharts (sample) {
-
+  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json")
+    .then((data) => {
+      
+    });
 }
 
 function init () {
   let selector = d3.select('#selDataset');
   d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json")
     .then((data) => {
-      let sampleNames = data.names;
+
+      let sampleNames = data.names;                  // an array of strings
+
+      for (let i = 0; i < sampleNames.length; i++) { // for each sample...
+        selector
+          .append('option')                          // ...create a tag `option` under tag `select`
+          .text(sampleNames[i])                      // with the sample's name as text
+          .property('value', sampleNames[i]);        // and set the property `value` to the sample's name
+      }
+
+      // plot initialization
+      let firstSample = sampleNames[0];
+      buildCharts  (firstSample);
+      buildMetadata(firstSample);
+
     });
 }
 
+// EVENT HANDLER
+// rebuild the plots each time a new sample is selected
 function optionChanged (newSample) {
-
+  buildCharts  (newSample);
+  buildMetadata(newSample);
 }
 
 init();
